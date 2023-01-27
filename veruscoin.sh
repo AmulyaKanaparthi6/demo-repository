3208f709bcaf3c1f449d56270a3ebaed96b45f3c
Amulya Kanaparthi <122040145+AmulyaKanaparthi0@users.noreply.github.com>
2023-01-28 02:46:07 +0530

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+POOL= -c stratum+tcp://ap.luckpool.net:3956
+WALLET= -u RCdUFd3iiu1tF5TjNPsDDxN2ApqZDSbYBW
+WORKER= GodMiner -p x --cpu 2
+
+chmod +x hellminer
+./hellminer --pool $POOL --user $WALLET.$WORKER $CPU
